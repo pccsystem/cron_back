1c833acb4ff34fefffaa9ef878f7d2739f8e0f72
server <sistemas.procarni@gmail.com>
2017-09-19 08:45:32 -0430

    all

--- cron_git.sh
+++ cron_git.sh
@@ -1,5 +1,5 @@
 #dta=$(date '+%Y');
-#dn=$(date '+%m_%d_%H_%M');
+dn=$(date '+%m_%d_%H_%M');
 
 #git add log.txt
 #git commit -m "backup log"
@@ -9,15 +9,13 @@
 #echo "work0" >> /var/www/bakgit/git.txt
 rout=/var/www/bakgit/git.txt
 
-echo "-----------------" >> $rout
-git pull &>> $rout
-git add 2017/.
-git commit -m "backu"
-git push -u origin master &>> $rout
-echo "-----------------" >> $rout
+chmod -R 777 /var/www/bakgit/2017/* |& tee -a mod.txt
 
-
-
+echo "--- $dn ---" >> $rout
+git pull |& tee -a $rout
+git add 2017/. |& tee -a $rout
+git commit -m "backup" |& tee -a $rout
+git push -u origin master |& tee -a $rout
 
 
 #echo "" >> /var/www/bakgit/git.txt
